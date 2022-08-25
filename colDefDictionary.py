cdd8edbb0bb7058228625781b3ed03ef74370c8a
Joshua McDonald <joshua.mcdonald@usradiology.com>
2022-08-25 12:56:46 -0400

    okay i toned it down a little on the column width

--- colDefDictionary.py
+++ colDefDictionary.py
@@ -220,7 +220,7 @@ def facilityBuild():
             },
             
            
-           {'field': 'AuditComment', 'width':1000, 'editable':True, 'suppressSizeToFit':True},
+           {'field': 'AuditComment', 'width':750, 'editable':True, 'suppressSizeToFit':True},
             
             
         ],
